37c333d70b2be2b0e8d7c12c88e8c2a70e89b03f
Charly Delay <charly@delay.gg>
2022-10-29 17:22:39 +0900

    Add EDITOR definition

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,9 +4,17 @@ if status is-interactive
     test -x /opt/homebrew/bin/brew && eval (/opt/homebrew/bin/brew shellenv)
     test -d $HOME/.local/bin && fish_add_path $HOME/.local/bin
 
+    set -gx EDITOR (which nvim)
+    set -gx VISUAL $EDITOR
+    set -gx SUDO_EDITOR $EDITOR
+
     set -Ux BAT_THEME TwoDark
     set -Ux FZF_DEFAULT_OPTS '--color=fg:#abb2bf,bg:#21252b,hl:#61afef --color=fg+:#abb2bf,bg+:#21252b,hl+:#61afef --color=info:#e5c07b,prompt:#e06c75,pointer:#e06c75 --color=marker:#98c379,spinner:#c678dd,header:#56b6c2'
-    set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH $HOME/.ghcup/bin # ghcup-env
+    set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
+    set -gx PATH $HOME/.cabal/bin $PATH $HOME/.ghcup/bin # ghcup-env
+
+    set -Ux HOMEBREW_NO_AUTO_UPDATE 1
+    set -Ux MANPAGER "nvim +Man!"
 
     # Set if your term supports `pipenv shell --fancy`.
     set pipenv_fish_fancy yes
@@ -50,4 +58,3 @@ if status is-interactive
         end
     end
 end
-
